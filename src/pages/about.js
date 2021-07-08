1a4c0e3a44185b9554eb4d54a8c7410f3b262bcd
sudhirtricore <sudhirtiwari@tricoreitsolutions.com>
2021-07-08 13:27:34 +0530

    my first commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Welcome to my Port.</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+  return (
+    <Layout pageTitle="Home Page">
+      <p>This is my Home Page.</p>
+    </Layout>
+  )
+}
+
+export default IndexPage
